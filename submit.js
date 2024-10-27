87e6c7f2e0982d0db86d1c56e2807c96ff543981
kavv3t <lamafofor@gmail.com>
2024-10-27 02:32:00 +0100

    ajout js pour gérer alert zapier

---
+++ submit.js
@@ -0,0 +1,21 @@
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = document.getElementById("contactForm");
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch(form.action, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok) {
+      alert("Votre message a été envoyé avec succès !");
+    } else {
+      alert("Erreur lors de l'envoi du formulaire. Veuillez réessayer.");
+    }
+  } catch (error) {
+    alert("Une erreur est survenue : ", + error.message);
+  }
+}
